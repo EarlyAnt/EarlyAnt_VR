b424e9c865fb0554c76a06aafe16234bae559073
EarlyAnt <54763755@qq.com>
2020-04-14 09:56:26 +0800

    1.上传Parkour项目。 2.初步实现陀螺仪控制相机转动。 3.下一步接入InControl插件。

---
+++ Parkour/Assets/Scripts/Observer.cs
@@ -0,0 +1,33 @@
+﻿using RootMotion;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    /************************************************属性与变量命名************************************************/
+    [SerializeField]
+    private Transform camera;
+    [SerializeField]
+    private Transform aircraft;
+    private Vector3 positionDiff;
+    /************************************************Unity方法与事件***********************************************/
+    void Start()
+    {
+        this.positionDiff = this.camera.position - this.aircraft.position;
+
+        Input.gyro.enabled = true;//打开陀螺仪权限
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Input.gyro.updateInterval = 0.02f;
+    }
+    void Update()
+    {
+        this.camera.transform.localRotation = CameraRotation();
+        this.camera.transform.position = this.aircraft.position + this.positionDiff;
+    }
+    /************************************************自 定 义 方 法************************************************/
+    Quaternion CameraRotation()
+    {
+        Quaternion input = Input.gyro.attitude;
+        input = Quaternion.Euler(90, 0, 0) * (new Quaternion(-input.x, -input.y, input.z, input.w));
+        return input;
+    }
+}
---
+++ Parkour/Assets/Scripts/Observer2.cs
@@ -0,0 +1,27 @@
+﻿using RootMotion;
+using UnityEngine;
+
+public class Observer2 : MonoBehaviour
+{
+    /************************************************属性与变量命名************************************************/
+    [SerializeField]
+    private Transform camera;
+    /************************************************Unity方法与事件***********************************************/
+    void Start()
+    {
+        Input.gyro.enabled = true;//打开陀螺仪权限
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Input.gyro.updateInterval = 0.02f;
+    }
+    void Update()
+    {
+        this.camera.transform.localRotation = CameraRotation();
+    }
+    /************************************************自 定 义 方 法************************************************/
+    Quaternion CameraRotation()
+    {
+        Quaternion input = Input.gyro.attitude;
+        input = Quaternion.Euler(90, 0, 0) * (new Quaternion(-input.x, -input.y, input.z, input.w));
+        return input;
+    }
+}
